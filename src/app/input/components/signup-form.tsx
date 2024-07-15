0148f1cc455f53cdcdc729c2b83237804da8cee3
Sumithsigtia <137398119+Sumithsigtia@users.noreply.github.com>
2024-07-15 11:47:13 +0530

    Update signup-form.tsx

--- src/app/input/components/signup-form.tsx
+++ src/app/input/components/signup-form.tsx
@@ -73,7 +73,7 @@ function SignupFormDemo({ nextStep, setPreferences }: SignupFormProps) {
               className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
               type="submit"
             >
-              Sign up &rarr;
+              Generate Plan &rarr;
               <BottomGradient />
             </button>
           </form>
