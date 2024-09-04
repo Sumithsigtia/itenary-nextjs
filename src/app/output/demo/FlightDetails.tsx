2fc04f9456ebf4bdd4f78407df9c00c2108e0c60
Sumithsigtia <137398119+Sumithsigtia@users.noreply.github.com>
2024-09-04 11:03:36 +0530

    Update FlightDetails.tsx

--- src/app/output/demo/FlightDetails.tsx
+++ src/app/output/demo/FlightDetails.tsx
@@ -15,7 +15,7 @@ interface FlightData {
 
 const FlightDetails: React.FC<FlightDetailsProps> = ({ source, destination, departureDate }) => {
   const [flightData, setFlightData] = useState<FlightData[]>([]);
-  const baseUrl = 'https://trademarks-reload-afford-debug.trycloudflare.com';
+  const baseUrl = 'https://respondents-donor-wage-mechanics.trycloudflare.com';
 
   const fetchFlightDetails = async () => {
     try {
